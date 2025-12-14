8b646937d6ad3aa062aee90d0d8e263e47cb81a8
Rello <Rello@users.noreply.github.com>
2025-12-14 23:35:34 +0700

    Optimize tenancy partner hydration

--- lib/Db/PartnerMapper.php
+++ lib/Db/PartnerMapper.php
@@ -5,6 +5,7 @@ namespace OCA\Domus\Db;
 use OCP\AppFramework\Db\QBMapper;
 use OCP\DB\Exception;
 use OCP\IDBConnection;
+use OCP\DB\QueryBuilder\IQueryBuilder;
 
 class PartnerMapper extends QBMapper {
     public function __construct(IDBConnection $db) {
@@ -38,5 +39,27 @@ class PartnerMapper extends QBMapper {
             ->andWhere($qb->expr()->eq('user_id', $qb->createNamedParameter($userId)));
 
         return $this->findEntity($qb);
+    }
+
+    /**
+     * @param int[] $ids
+     *
+     * @return Partner[]
+     *
+     * @throws Exception
+     */
+    public function findForUserByIds(array $ids, string $userId): array {
+        $ids = array_values(array_unique(array_map('intval', $ids)));
+        if ($ids === []) {
+            return [];
+        }
+
+        $qb = $this->db->getQueryBuilder();
+        $qb->select('*')
+            ->from($this->getTableName())
+            ->where($qb->expr()->eq('user_id', $qb->createNamedParameter($userId)))
+            ->andWhere($qb->expr()->in('id', $qb->createNamedParameter($ids, IQueryBuilder::PARAM_INT_ARRAY)));
+
+        return $this->findEntities($qb);
     }
 }
