fbf9f549dd19166351ac212a537fabfeeb2826e8
Rello <github@scherello.de>
2026-03-25 22:50:04 +0100

    fixes

---
+++ templates/partials/splash.php
@@ -0,0 +1,15 @@
+<div
+    id="app-splash-screen"
+    class="app-splash-screen"
+    data-splash
+    data-draw-ms="2000"
+    data-reveal-ms="420"
+    data-hold-ms="1000"
+    data-fade-ms="320"
+    aria-hidden="true"
+>
+    <div class="app-splash-content">
+        <div class="app-splash-logo" data-splash-logo aria-hidden="true"></div>
+        <div class="app-splash-name" data-splash-name></div>
+    </div>
+</div>
